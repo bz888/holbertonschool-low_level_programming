b4140361bdeae41b76b2719c36bd5676a185af5d
bz888 <ziyu.ben158@gmail.com>
2026-02-09 22:39:32 +1100

    betty stop

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
  * main - Prints
@@ -10,27 +10,25 @@
 int main(void)
 {
 	int n;
-  int l;
+	int l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-  l = n % 10;
 
-  if (l == 0)
+	l = n % 10;
+
+	if (l == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
-    return (0);
 	}
-  
-  if (l > 5) 
-  {
+	else if (l > 5)
+	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
-  }
-  else
-  {
+	}
+	else
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-  }
-  
+	}
+
 	return (0);
 }
---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  
+	return (0);
+}

5049783905bdf65f4b76d5189d5d6137a3667c12
bz888 <ziyu.ben158@gmail.com>
2026-02-09 23:25:43 +1100

    b

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Prints
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar('0' + (a / 10));
+			putchar('0' + (a % 10));
+			putchar(' ');
+			putchar('0' + (b / 10));
+			putchar('0' + (b % 10));
+
+			if (a != 98 || b != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

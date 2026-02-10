e0c11527b24def27c74570a532e272fbab5adc70
bz888 <ziyu.ben158@gmail.com>
2026-02-10 17:38:01 +1100

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - prints
+ * @c: character (int)
+ *
+ * Return: 0 or 1.
+ */
+int _islower(int c)
+{
+	if (c < 'a' && c > 'z')
+	{
+		return (0);
+	}
+	return (0);
+}
+

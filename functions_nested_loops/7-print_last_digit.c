2abc95c812b2e257b070eac2d1d43fdd8f2e9f1f
bz888 <ziyu.ben158@gmail.com>
2026-02-10 18:16:11 +1100

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_last_digit - does things
+ *@n: int
+ *
+ * Return: int
+ */
+int print_last_digit(int n)
+{
+	int l = n % 10;
+
+	if (l < 0)
+		l = -l;
+	_putchar('0' + l);
+	return (l);
+}
+

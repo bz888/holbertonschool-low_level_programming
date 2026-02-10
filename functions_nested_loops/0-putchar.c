faeaea7c0e2a1e902e63c43ba98a5d671b50b87b
bz888 <ziyu.ben158@gmail.com>
2026-02-10 17:07:29 +1100

    all

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s[] = "_putchar\n";
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+
+	return (0);
+}
+

c165296c7e192614621dcd3e8b1d28ddf74673af
bz888 <ziyu.ben158@gmail.com>
2026-02-17 21:27:03 +1100

    8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i != 0)
+			printf(", ");
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
+}
+

08ee38fe3fb9af94e6bff9a4130cb3861e06ca53
bz888 <ziyu.ben158@gmail.com>
2026-02-17 21:49:31 +1100

    lol

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <limits.h>
+
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, res = 0, found = 0, d;
+
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign = -sign;
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		found = 1;
+		d = s[i] - '0';
+
+		if (sign == 1)
+		{
+			if (res > (INT_MAX - d) / 10)
+				return (INT_MAX);
+			res = res * 10 + d;
+		}
+		else
+		{
+			if (res < (INT_MIN + d) / 10)
+				return (INT_MIN);
+			res = res * 10 - d;
+		}
+		i++;
+	}
+
+	return (found ? res : 0);
+}
+

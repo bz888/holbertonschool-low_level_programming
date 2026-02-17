f8880d97996c5b473613bf8e203deee609d2d916
bz888 <ziyu.ben158@gmail.com>
2026-02-17 21:11:53 +1100

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,13 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int len = 0, i = 0;
 
-	if (str == 0)
-	{
-		_putchar('\n');
-		return;
-	}
 
-	while (str[i] != '\0')
+	while (str[len])
+		len++;
+
+	while (i < len)
 	{
 		_putchar(str[i]);
 		i += 2;

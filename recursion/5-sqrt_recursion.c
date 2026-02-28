fcd70c9ba4d94fc79ecdbf0d49613819512065c4
bz888 <ziyu.ben158@gmail.com>
2026-02-28 14:06:07 +1100

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,29 +1,42 @@
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * lookup_sqrt - searches for natural sqrt using binary search
+ * @n: number
+ * @low: lower bound
+ * @high: upper bound
+ *
+ * Return: natural sqrt or -1
+ */
+int lookup_sqrt(int n, int low, int high)
+{
+	int mid;
+
+	if (low > high)
+		return (-1);
+
+	mid = low + (high - low) / 2;
+
+	if (mid == 0)
+		return (-1);
+
+	if (n / mid == mid && n % mid == 0)
+		return (mid);
+	else if (n / mid < mid)
+		return (lookup_sqrt(n, low, mid - 1));
+	else
+		return (lookup_sqrt(n, mid + 1, high));
+}
+
+/**
+ * _sqrt_recursion - returns natural square root
  * @n: number to evaluate
  *
- * Return: natural square root or -1
+ * Return: sqrt or -1
  */
 int _sqrt_recursion(int n)
 {
-	int i = 1;
-
 	if (n < 0)
 		return (-1);
 
-	if (i * i == n)
-	{
-		i = 1;
-		return (i * i == n ? i : i); /* return i */
-	}
-
-	if (i * i > n)
-	{
-		i = 1;
-		return (-1);
-	}
-
-	i++;
-	return (_sqrt_recursion(n));
+	return (lookup_sqrt(n, 0, n));
 }
 

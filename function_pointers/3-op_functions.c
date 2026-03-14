3dad3e7db6e9a4c0c591c25d4155ac6fb666a10e
bz888 <ziyu.ben158@gmail.com>
2026-03-14 16:25:06 +1100

    3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+/**
+ * op_add - add two integers
+ * @a: addend
+ * @b: addend
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - subtract an integer from another
+ * @a: minuend
+ * @b: subtrahend
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - multiply two integers
+ * @a: multiplicand
+ * @b: multiplier
+ *
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - divide an integer by another
+ * @a: dividend
+ * @b: divisor
+ *
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - modulo divide an integer by another
+ * @a: dividend
+ * @b: modulus
+ *
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
+
+
+

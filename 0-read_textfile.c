1bc08c2e7564a84f3b5fa21f80bda3cf93750a13
bz888 <ziyu.ben158@gmail.com>
2026-03-29 02:32:21 +1100

    0

---
+++ 0-read_textfile.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: name of file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters printed, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t r, w;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	w = write(STDOUT_FILENO, buf, r);
+	if (w != r)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+
+	return (w);
+}
+

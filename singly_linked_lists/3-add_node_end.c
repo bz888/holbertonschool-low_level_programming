cca14abe99ba27da438a119d9714c80daf53add0
bz888 <ziyu.ben158@gmail.com>
2026-03-29 00:36:04 +1100

    3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,60 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _strlen - finds length of a string
+ * @s: string to be measured
+ *
+ * Return: length of string
+ */
+size_t _strlen(const char *s)
+{
+	size_t len = 0;
+
+	while (s && *s)
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * _create_node - creates a new node
+ * @s: string value of node
+ * @next: pointer to next node
+ *
+ * Return: created node, or NULL if it fails
+ */
+list_t *_create_node(const char *s, list_t *next)
+{
+	list_t *node;
+
+	node = malloc(sizeof(list_t));
+	if (!node)
+		return (NULL);
+
+	if (!s)
+	{
+		node->str = NULL;
+		node->len = 0;
+	}
+	else
+	{
+		node->str = strdup(s);
+		if (!node->str)
+		{
+			free(node);
+			return (NULL);
+		}
+		node->len = _strlen(s);
+	}
+
+	node->next = next;
+
+	return (node);
+}
 
 /**
  * add_node_end - adds a new node at the end of a list_t list
@@ -14,30 +68,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 	list_t *temp;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	new_node = _create_node(str, NULL);
+	if (!new_node)
 		return (NULL);
-
-	if (str == NULL)
-	{
-		new_node->str = NULL;
-		new_node->len = 0;
-	}
-	else
-	{
-		new_node->str = strdup(str);
-		if (new_node->str == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		new_node->len = strlen(str);
-	}
-
-	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
@@ -46,7 +82,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	temp = *head;
-	while (temp->next != NULL)
+	while (temp->next)
 		temp = temp->next;
 
 	temp->next = new_node;
